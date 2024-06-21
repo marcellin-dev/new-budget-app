e949c44d6fb78f7da7dc6f9002ca422c11490147
dilane <dilanelele9@gmail.com>
2024-06-21 10:06:18 +0100

    création du formulaire login

--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -1,7 +1,55 @@
-export default function SignIn() {
+const SignIn = () => {
   return (
-    <div className="text-center text-4xl">
-      <h1>Sign In</h1>
+    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-sm">
+        <h2 className="text-2xl font-bold mb-6 text-center">SignIn</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email Address
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="********"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
+
+export default SignIn;
--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,9 +1,24 @@
 export default function NoPage() {
   return (
-    <div className=" flex justify-center border ">
-      <div className="mt-20  text-4xl">
-        <h1>404</h1>
-        <p>Page not found</p>
+    <div className="  justify-center  ">
+      <h1 className="text-white text-4xl font-bold py-2 px-4 rounded bg-blue-500 text-center">
+        BUDGET MANAGER
+      </h1>
+      <div className="mt-20 ">
+        <div className="mt-5 flex ">
+          <p className="w-1/2 bg-blue-500 py-2 px-20">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+            perspiciatis libero laborum. Recusandae ipsam sed quos, autem qui
+            molestiae nisi consectetur officia quisquam possimus, non
+            praesentium corrupti neque maiores harum. Maxime sequi officiis
+          </p>
+          <p className="w-1/2 bg-blue-300 py-2 px-20">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt
+            perspiciatis libero laborum. Recusandae ipsam sed quos, autem qui
+            molestiae nisi consectetur officia quisquam possimus, non
+            praesentium corrupti neque maiores harum. Maxime sequi officiis
+          </p>
+        </div>
       </div>
     </div>
   );
